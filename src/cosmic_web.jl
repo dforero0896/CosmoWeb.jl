239037d573cd8bd65726c6626919cb15cc275277
= <=>
2022-07-07 14:10:32 +0200

    Improved cosmic web computations

--- src/cosmic_web.jl
+++ src/cosmic_web.jl
@@ -43,107 +43,107 @@ function _rebin_field(field::Array{<:AbstractFloat, 3}, kernel_size::Tuple{<:Int
     #dropdims(sum(reshape(field, new_shape[1], kernel_size[1], new_shape[2], kernel_size[2], new_shape[3], kernel_size[3]), dims=(2, 4, 6)), dims=(2, 4, 6))
 end
 
+function tidal_field_ab!(a::Int, b::Int, ks::Vector{<:AbstractVector{T}}, delta_k::Array{<:Complex{T}, 3}, smoothing::T, tidal_comp::Array{<:Complex{T}, 3}) where T<:AbstractFloat
+    R = CartesianIndices(delta_k)
+    for I in R
+        i, j, k = Tuple(I)
+        ksq = ks[1][i]^2 + ks[2][j]^2 + ks[3][k]^2
+        if ksq > 0.
+            tidal_comp[I] = - ks[a][I[a]] * ks[b][I[b]] * delta_k[I] * exp(-0.5 * 2 * pi * ksq * smoothing^2) / ksq
+        else
+            tidal_comp[I] = 0.
+        end
+    end
+end
 
-function compute_tidal_field(delta::Array{<:AbstractFloat, 3}, box_size::SVector{3}, smoothing::AbstractFloat)
+
+function compute_tidal_field(delta::Array{<:T, 3}, box_size::SVector{3, T}, smoothing::T) where T<:AbstractFloat
+
     n_bins = size(delta)
     @show n_bins
 
     init_k = rfft(delta)
     init_k[1,1,1] = 0.
+    irfft_plan = plan_irfft(init_k, size(delta,1))
 
-    kx = rfftfreq(n_bins[1], Float32(n_bins[1] / box_size[1] .* 2 * pi))
-    ky = fftfreq(n_bins[2], Float32(n_bins[2] / box_size[2] .* 2 * pi))
-    kz = ky
+    kx::Vector{T} = rfftfreq(n_bins[1], T(n_bins[1] / box_size[1] .* 2 * pi))
+    ky::Vector{T} = fftfreq(n_bins[2], T(n_bins[2] / box_size[2] .* 2 * pi))
+    kz::Vector{T} = ky
     
 
     ks = [kx, ky, kz]
 
-    
-    
-    Threads.@threads for i in 1:length(kx)
-        for j in 1:length(ky)
-            for k in 1:length(kz)
-                ksq = kx[i]^2 + ky[j]^2 + kz[k]^2
-                init_k[i,j,k] *= exp(-0.5 * 2 * pi * ksq * smoothing^2)
-                if ksq > 0.
-                    init_k[i,j,k] /= ksq
-                else
-                    init_k[i,j,k] = 0.
-                end
-    
-                #tidal_field[k,j,i, 1, 1] = - kx[i] * kx[i] * init_k[k,j,i]
-                #tidal_field[k,j,i, 1, 2] = - kx[i] * ky[j] * init_k[k,j,i]
-                #tidal_field[k,j,i, 1, 3] = - kx[i] * kz[k] * init_k[k,j,i]
-#
-                #tidal_field[k,j,i, 2, 1] = tidal_field[k,j,i, 1, 2]
-                #tidal_field[k,j,i, 2, 2] = - ky[j] * ky[j] * init_k[k,j,i]
-                #tidal_field[k,j,i, 2, 3] = - ky[j] * kz[k] * init_k[k,j,i]
-#
-                #tidal_field[k,j,i, 3, 1] = tidal_field[k,j,i, 1, 3]
-                #tidal_field[k,j,i, 3, 2] = tidal_field[k,j,i, 2, 3]
-                #tidal_field[k,j,i, 3, 3] = - kz[k] * kz[k] * init_k[k,j,i]
-            end
-        end
-    end
-
     k_space_buffer = similar(init_k)
     tidal_field = [Array{Float32, 3}(undef, size(delta)) for i=1:6]
     
-    newaxis = [CartesianIndex()]
+    
     #
     # 1,1 element is index 1
-    k_space_buffer .= [- kx[i] * kx[i] * init_k[i,j,k] for i=1:size(init_k,1), j=1:size(init_k,2), k=1:size(init_k,3)]
-    tidal_field[1] .= irfft(k_space_buffer, size(delta, 1))
+    tidal_field_ab!(1, 1, ks, init_k, smoothing, k_space_buffer)
+    mul!(tidal_field[1], irfft_plan, k_space_buffer)
     # 2,1 element is index 2
-    k_space_buffer .= [- ky[j] * kx[i] * init_k[i,j,k] for i=1:size(init_k,1), j=1:size(init_k,2), k=1:size(init_k,3)]
-    tidal_field[2] .= irfft(k_space_buffer, size(delta, 1))
+    tidal_field_ab!(2, 1, ks, init_k, smoothing, k_space_buffer)
+    mul!(tidal_field[2], irfft_plan, k_space_buffer)
     # 3,1 element is index 3
-    k_space_buffer .= [- kz[k] * kx[i] * init_k[i,j,k] for i=1:size(init_k,1), j=1:size(init_k,2), k=1:size(init_k,3)]
-    tidal_field[3] .= irfft(k_space_buffer, size(delta, 1))
+    tidal_field_ab!(3, 1, ks, init_k, smoothing, k_space_buffer)
+    mul!(tidal_field[3], irfft_plan, k_space_buffer)
     # 2,2 element is index 4
-    k_space_buffer .= [- ky[j] * ky[j] * init_k[i,j,k] for i=1:size(init_k,1), j=1:size(init_k,2), k=1:size(init_k,3)]
-    tidal_field[4] .= irfft(k_space_buffer, size(delta, 1))
+    tidal_field_ab!(2, 2, ks, init_k, smoothing, k_space_buffer)
+    mul!(tidal_field[4], irfft_plan, k_space_buffer)
     # 3,2 element is index 5
-    k_space_buffer .= [- kz[k] * ky[j] * init_k[i,j,k] for i=1:size(init_k,1), j=1:size(init_k,2), k=1:size(init_k,3)]
-    tidal_field[5] .= irfft(k_space_buffer, size(delta, 1))
+    tidal_field_ab!(3, 2, ks, init_k, smoothing, k_space_buffer)
+    mul!(tidal_field[5], irfft_plan, k_space_buffer)
     # 3.3 element is index 6 
-    k_space_buffer .= [- kz[k] * kz[k] * init_k[i,j,k] for i=1:size(init_k,1), j=1:size(init_k,2), k=1:size(init_k,3)]
-    tidal_field[6] .= irfft(k_space_buffer, size(delta, 1))
+    tidal_field_ab!(3, 3, ks, init_k, smoothing, k_space_buffer)
+    mul!(tidal_field[6], irfft_plan, k_space_buffer)
 
     
+    tidal_field
+
+end
+
+
+function compute_tidal_eigenvalues(delta::Array{<:T, 3}, box_size::SVector{3, T}, smoothing::T) where T<:AbstractFloat
+
+    tidal_field = compute_tidal_field(delta, box_size, smoothing)
+
     matrix_buffer = Matrix{Float32}(undef, (3,3))
     vector_buffer = Vector{Float32}(undef, 3)
     eigenvalues = [Array{Float32, 3}(undef, size(delta)) for i=1:3]
-    Threads.@threads for i in 1:size(delta, 2)
-        for j in 1:size(delta, 2)
-            for k in 1:size(delta,3)
-                matrix_buffer[1,1] = tidal_field[1][i,j,k]
-                matrix_buffer[2,1] = tidal_field[2][i,j,k]
-                matrix_buffer[3,1] = tidal_field[3][i,j,k]
-                matrix_buffer[2,2] = tidal_field[4][i,j,k]
-                matrix_buffer[3,2] = tidal_field[5][i,j,k]
-                matrix_buffer[3,3] = tidal_field[6][i,j,k]
-                vector_buffer .= eigvals!(Hermitian(matrix_buffer,:L))
+    for I in CartesianIndices(delta)
+        i, j, k = Tuple(I)
+        matrix_buffer[1,1] = tidal_field[1][i,j,k]
+        matrix_buffer[2,1] = tidal_field[2][i,j,k]
+        matrix_buffer[3,1] = tidal_field[3][i,j,k]
+        matrix_buffer[2,2] = tidal_field[4][i,j,k]
+        matrix_buffer[3,2] = tidal_field[5][i,j,k]
+        matrix_buffer[3,3] = tidal_field[6][i,j,k]
+        vector_buffer .= eigvals!(Hermitian(matrix_buffer,:L))
 
-                eigenvalues[1][i,j,k] = vector_buffer[3]
-                eigenvalues[2][i,j,k] = vector_buffer[2]
-                eigenvalues[3][i,j,k] = vector_buffer[1]
-            end
-        end
+        eigenvalues[1][i,j,k] = vector_buffer[3]
+        eigenvalues[2][i,j,k] = vector_buffer[2]
+        eigenvalues[3][i,j,k] = vector_buffer[1]
     end
 
-                        
-
-
-    
-    
-
     eigenvalues
-
 end
 
-function compute_tidal_eigvals(tidal_field::Array{<:Real})
 
-    
+function compute_tidal_invariants(delta::Array{<:T, 3}, box_size::SVector{3, T}, smoothing::T) where T<:AbstractFloat
+
+    eigenvalues = compute_tidal_eigenvalues(delta, box_size, smoothing)
+    invariants = [zeros(T, size(delta)) for _ in 1:5]
+
+    for I in CartesianIndices(delta)
+        i,j,k = Tuple(I)
+        invariants[1][I] = eigenvalues[1][I] + eigenvalues[2][I] + eigenvalues[3][I]
+        invariants[2][I] = eigenvalues[1][I] * eigenvalues[1][I] + eigenvalues[3][I] * eigenvalues[2][I] * eigenvalues[3][I]
+        invariants[3][I] = eigenvalues[1][I] * eigenvalues[2][I] * eigenvalues[3][I]
+        invariants[4][I] = eigenvalues[1][I]^2 + eigenvalues[2][I]^2 + eigenvalues[3][I]^2
+        invariants[5][I] = eigenvalues[1][I]^3 + eigenvalues[2][I]^3 + eigenvalues[3][I]^3
+        invariants[6][I] = invariants[1][I] * invariants[2][I]
+    end
+    invariants
 end
+
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,11 @@ using Test
 using NPZ
 using StaticArrays
 using FortranFiles
-using UnicodePlots
+using Plots
 using Statistics
 using CSV
 using Profile
+using Zygote
 #pyplot()
 
 @testset "CosmoWeb.jl" begin
@@ -34,17 +35,29 @@ hr_name = "/data5/UNITSIM/1Gpc_4096/fixedAmp_InvPhase_001/DM_DENS/dmdens_cic_128
 
 @time delta = read_fortran_field(hr_name)
 println("Rebinning field...")
-@time delta = CosmoWeb._rebin_field(delta, (2, 2, 2))
+@time delta = CosmoWeb._rebin_field(delta, (16, 16, 16))
 println("Normalizing field...")
 delta ./= mean(delta)
 delta .-= 1.
-#field_size = 1024
+#field_size = 128
 #delta = randn(Float32, (field_size,field_size,field_size))
+box_size = SVector{3}(1f3,1f3,1f3)
+
+p = heatmap(dropdims(mean(delta, dims=1), dims=1), aspect = 1)
+savefig(p, "/home2/dfelipe/codes/CosmoWeb/test/test.png")
+
 
 println("Computing Pk of field...")
-@time k_edges, pk, _, n_modes = CosmoWeb.powspec_fundamental(delta, SVector{3}(1f3,1f3,1f3), Float32(pi * size(delta,1) / 1000.))
+@time k_edges, pk, _, n_modes = CosmoWeb.powspec_fundamental(delta, box_size, Float32(pi * size(delta,1) / 1000.))
+p2 = plot(k_edges[2:end], k_edges[2:end].*pk[1,2:end], xscale=:log10)
+fin_plot = plot(p, p2, dpi=200, size=(1000,1000), layout = (2,2))
+savefig(fin_plot, "/home2/dfelipe/codes/CosmoWeb/test/test.png")
 CSV.write("/home2/dfelipe/codes/CosmoWeb/test/test.csv", (k=k_edges, pk0=pk[1,:], pk2=pk[2,:], nmodes=n_modes))
 
 
-#@time eigenvalues = CosmoWeb.compute_tidal_field(delta, SVector{3}(1000., 1000., 1000.), 10.)
+@time invariants = CosmoWeb.compute_tidal_invariants(delta, box_size, 0f0);
+
+delta_lin = invariants[1];
+p3 = heatmap(dropdims(mean(-delta_lin, dims=1), dims=1), aspect = 1)
+fin_plot = plot(p, p3, p2, dpi=200, size=(1000,1000), layout = (2,2))
 
